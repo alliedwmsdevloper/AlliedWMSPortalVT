1e5c4f113b089b385d985e5744ca39491aa7ca12
ubuntu88 <shiferawdawit3@gmail.com>
2021-03-10 18:36:29 -0500

    adjustment to the drivers

--- src/test/java/com/wmsportalvt/testCases/TestBaseClass.java
+++ src/test/java/com/wmsportalvt/testCases/TestBaseClass.java
@@ -13,8 +13,11 @@ import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -42,19 +45,22 @@ public class TestBaseClass {
 		
 					if(br.equals("firefox"))
 						{
+						    FirefoxOptions fxOptions = new FirefoxOptions();
+						    fxOptions.setHeadless(true);
 							System.setProperty("webdriver.gecko.driver", config.getFirefoxPath() );
 							driver = new FirefoxDriver();
 						}
 					else if(br.equals("ie"))
 						{
-							
-
+					
 							System.setProperty("webdriver.ie.driver", config.getIEPath() );
 							driver = new InternetExplorerDriver();
 							
 						}
 					else if(br.equals("chrome"))
 						{
+						ChromeOptions chromeOptions = new ChromeOptions();
+						chromeOptions.setHeadless(true);
 						System.setProperty("webdriver.chrome.driver", config.getChromePath() );
 						driver=new ChromeDriver();
 						}
